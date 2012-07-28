2b06807215f980d68751bfb7ecf2c84ec2fdf2e8
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2012-07-28 23:53:00 +0200

    Bump version to 1.0.3.0

--- RandomImgur/RandomImgur.fs
+++ RandomImgur/RandomImgur.fs
@@ -13,7 +13,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyCopyright("Copyright ©  2012")>]
 [<assembly: AssemblyTrademark("")>]
 
-[<assembly: AssemblyVersion("1.0.2.1")>]
+[<assembly: AssemblyVersion("1.0.3.0")>]
 
 [<STAThread>]
 do Application.Run(new MainForm())
